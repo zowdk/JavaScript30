c2b64442edf2585b2b2e73592f724e42aac83892
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-18 13:53:21 +0200

    update style properties of variables

--- projects/03 - CSS Variables/index.js
+++ projects/03 - CSS Variables/index.js
@@ -3,6 +3,11 @@ const inputs = document.querySelectorAll(".controls input");
 function handleUpdate() {
   //what is the suffix of the value that we're working with?
   const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 // trigger change when input value is moved
