19e68a302882959967f5836e5cb523d1fd19789c
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-18 13:46:39 +0200

    get suffix for size of values

--- projects/03 - CSS Variables/index.js
+++ projects/03 - CSS Variables/index.js
@@ -1,8 +1,10 @@
 const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
-  console.log(this.value);
+  //what is the suffix of the value that we're working with?
+  const suffix = this.dataset.sizing || "";
 }
 
+// trigger change when input value is moved
 inputs.forEach((input) => input.addEventListener("change", handleUpdate));
 inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
