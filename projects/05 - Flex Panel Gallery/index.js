ac38ed278d0cae5fd2cc185554b959dd0f8c1ec7
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-20 14:08:03 +0200

    to toggle class of open on panels

---
+++ projects/05 - Flex Panel Gallery/index.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panels");
+
+// write a function that will toggle the class of open on panels:
+// listen for panel to be hovered over or clicked?
+// add class of .panel-open-active to first and last children in panel
+// remove class of .panel-open-active
+
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
