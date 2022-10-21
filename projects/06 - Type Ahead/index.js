c0bdc4540a887327daf34047359c5878e20e3849
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-21 11:51:59 +0200

    fetch raw data

--- projects/06 - Type Ahead/index.js
+++ projects/06 - Type Ahead/index.js
@@ -1,2 +1,12 @@
+// data was converted to a url from a cites.json file (array), which includes city name, state name, city coordinates, and population.
+
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+//set up array to store cities
+// use fetch api to get data
+const cities = [];
+
+const prom = fetch(endpoint).then((blob) =>
+  console.log(blob.json()).then((data) => console.log(data))
+);
