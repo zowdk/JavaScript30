b2f39b62cd7c16ccac0bedeefa6c1369114662be
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-21 12:19:01 +0200

    display matches when key is released

--- projects/06 - Type Ahead/index.js
+++ projects/06 - Type Ahead/index.js
@@ -34,3 +34,4 @@ const searchInput = document.querySelector(".search");
 const suggestions = document.querySelector(".suggestions");
 
 searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
