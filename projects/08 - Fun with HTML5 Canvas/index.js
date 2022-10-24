aff744e66332c1dacd2d55909ce3355ae1dd9164
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-24 21:09:24 +0200

    for better organization/ readability

--- projects/08 - Fun with HTML5 Canvas/index.js
+++ projects/08 - Fun with HTML5 Canvas/index.js
@@ -9,6 +9,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADA55"; // drawing color
 ctx.lineJoin = "round"; // end of line drawn should be round
 ctx.lineCap = "round"; // round line when it meets another line
+ctx.lineWidth = 100;
 
 // dummy variables
 
@@ -22,17 +23,17 @@ function draw(e) {
   if (!isDrawing) return;
   console.log(e);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
 
   //start from
   ctx.moveTo(lastX, lastY);
   //go to
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
-  ctx.lineWidth = 100;
-
   //written in fancy es6 destructuring [lastX, lastY] = [e.offsetX, e.offsetY];
   lastX = e.offsetX;
   lastY = e.offsetY;
+
   hue++;
   if (hue >= 360) {
     hue = 0;
