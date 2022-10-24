4b43e08b6db342d4cc17edf3806ff48c0b94b0d4
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-24 17:19:23 +0200

    to know when user is not drawing

--- projects/08 - Fun with HTML5 Canvas/index.js
+++ projects/08 - Fun with HTML5 Canvas/index.js
@@ -24,3 +24,4 @@ function draw(e) {
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mousedown", () => (isDrawing = true));
 canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
