3ac764df3b38438d4416e440bdd4cda73a2e791b
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-24 17:03:11 +0200

    set up base styles for drawing stoke

--- projects/08 - Fun with HTML5 Canvas/index.js
+++ projects/08 - Fun with HTML5 Canvas/index.js
@@ -4,3 +4,8 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// base styles for stoke setting, line cap, line join
+ctx.strokeStyke = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
