d82681e5d3cd92c39aa1f77c2b2ce7b2aea2c4be
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-27 10:23:29 +0200

    get reference to input last checked

--- projects/10 - Hold Shift and Check Checkboxes/index.js
+++ projects/10 - Hold Shift and Check Checkboxes/index.js
@@ -6,6 +6,10 @@
 const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
 console.log(checkboxes);
 
+let lastChecked = function handleCheck(e) {
+  lastChecked = this;
+};
+
 checkboxes.forEach((checkbox) =>
   checkbox.addEventListener("click", handleCheck)
 );
