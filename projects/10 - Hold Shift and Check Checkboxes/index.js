96e55cd4848a2e3637f112f5acdf4a45769a6096
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-27 10:16:32 +0200

    instructions + setup listeners on checkboxes

---
+++ projects/10 - Hold Shift and Check Checkboxes/index.js
@@ -0,0 +1,17 @@
+// Write an application that allows the user to select multiple checkboxes at once:
+// When user clicks one checkbox, then clicks another while holding shift,
+// all boxes between the (2) boxes clicked should be selected.
+// Including the (2) boxes clicked.
+
+const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
+console.log(checkboxes);
+
+// checkbox.addEventListener("onclick", selectStart);
+
+// function selectStart() {
+//   console.log("it works!");
+// }
+
+// function selectEnd() {
+//   KeyboardEvent.shiftKey();
+// }
