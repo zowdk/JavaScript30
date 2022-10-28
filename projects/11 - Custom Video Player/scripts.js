56a3ddbde6678c1591ff92e3a4494b39a12cd800
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-10-28 18:01:43 +0200

    handle sliders for volume + playback speed

--- projects/11 - Custom Video Player/scripts.js
+++ projects/11 - Custom Video Player/scripts.js
@@ -7,7 +7,7 @@ const playerToggle = player.querySelector(".toggle");
 const playerSlider = player.querySelectorAll(".player__slider");
 const skipButtons = player.querySelectorAll("[data-skip]");
 
-//2. Build functions
+//2. FUNCTIONS
 
 //Write a function that plays a paused video,
 // or pauses a playing video.
@@ -33,7 +33,12 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
-//3. Hook up event listeners
+//Write a function that handles changes with the range inputs
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+//3. EVENT LISTENERS
 
 //when play button or video screen get clicked, togglePlay
 video.addEventListener("click", togglePlay);
@@ -45,3 +50,11 @@ video.addEventListener("pause", updateButton);
 
 //listen for a click on anything with data-skip attribute
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+//listen for changes AND mousemove events on sliders/ range inputs
+playerSlider.forEach((playerSlider) =>
+  playerSlider.addEventListener("change", handleRangeUpdate)
+);
+playerSlider.forEach((playerSlider) =>
+  playerSlider.addEventListener("mousemove", handleRangeUpdate)
+);
