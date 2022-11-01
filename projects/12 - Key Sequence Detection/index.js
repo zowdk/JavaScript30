b1d033acdb193b802bf0083b34c5f63204d774a5
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-11-01 09:48:27 +0100

    listen for user to type a key sequence

---
+++ projects/12 - Key Sequence Detection/index.js
@@ -0,0 +1,14 @@
+//basis of setting up a key sequencing detection
+
+let pressed = [];
+const secretCode = "wesbos";
+
+window.addEventListener("keyup", (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  console.log(pressed);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); //search only for char length of secretcode, not log every key pressed
+  if (pressed.join("").includes(secretCode)) {
+    console.log("DING DING! Got it!");
+  }
+});
