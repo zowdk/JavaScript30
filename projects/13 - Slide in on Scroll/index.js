f2c1f64eb14b9a0ab6aaeb83bfbb05b6bb41d5e3
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-11-01 10:38:59 +0100

    slide in images at mid-point when scrolling

--- projects/13 - Slide in on Scroll/index.js
+++ projects/13 - Slide in on Scroll/index.js
@@ -20,7 +20,14 @@ const sliderImages = document.querySelectorAll(".slide-in");
 //Write a function checkSlide that runs every time user scrolls
 function checkSlide(e) {
   console.log(e);
+  //loop over every img, figure out where each img needs to be displayed
+  sliderImagees.array.forEach((sliderImage) => {
+    //do math to figure out if image is displayed or not:
+    //how far is page being scrolled down? get pixel level for where user is relative to bottom of page:
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2; //slide img in at mid-point
+    console.log(slideInAt);
+  });
 }
 //  use debounce to limit how much this check runs. hundreds of events can cause performance issues:
-
 window.addEventListener("scoll", debounce(checkSlide));
