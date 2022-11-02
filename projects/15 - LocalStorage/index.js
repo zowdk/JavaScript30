89186c71de72b03dea48e48317c78c415b0293f9
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-11-02 11:28:14 +0100

    listen for submit event

--- projects/15 - LocalStorage/index.js
+++ projects/15 - LocalStorage/index.js
@@ -1,3 +1,10 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function addItem(e) {
+  console.log("hello");
+}
+
+// listen for submit event
+addItems.addEventListener("submit", addItem);
