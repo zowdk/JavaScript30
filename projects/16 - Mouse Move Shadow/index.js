b234a2ff018802cb30c8230e7733f2a2e051f3dc
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-11-03 09:13:25 +0100

    add styling to shadow

--- projects/16 - Mouse Move Shadow/index.js
+++ projects/16 - Mouse Move Shadow/index.js
@@ -20,6 +20,13 @@ function shadow(e) {
   const xWalk = Math.round((x / width) * walk - walk / 2);
   const yWalk = Math.round((y / height) * walk - walk / 2);
 
+  text.style.textShadow = `
+  ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+  ${xWalk * -1}px ${yWalk}px 0 rgba(0, 255, 255, 0.7),
+  ${yWalk}px ${xWalk}px 0 rgba(0, 255, 0, 0.7),
+  ${yWalk * -1}px ${xWalk}px 0 rgba(0, 0, 255, 0.7),
+  `;
+
   console.log(x, y);
 }
 
