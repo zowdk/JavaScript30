df486766ab85392d436f500cbd5544d882a68dd8
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-11-03 08:43:26 +0100

    get height and width of hero element

--- projects/16 - Mouse Move Shadow/index.js
+++ projects/16 - Mouse Move Shadow/index.js
@@ -2,7 +2,11 @@ const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
 
 function shadow(e) {
-  console.log(e);
+  //get width and height of the hero using destructuring
+  //   const width = hero.offsetWidth;
+  //   const height = hero.offsetHeight;
+
+  const { offsetWidth: width, offsetHeight: height } = hero;
 }
 
 hero.addEventListener("mousemove", shadow);
