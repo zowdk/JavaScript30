8edae533a3e9c1a5c207fe939575fac984e135c3
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-11-03 08:51:07 +0100

    modify x + y values when hovering over hero and its children

--- projects/16 - Mouse Move Shadow/index.js
+++ projects/16 - Mouse Move Shadow/index.js
@@ -7,6 +7,14 @@ function shadow(e) {
   //   const height = hero.offsetHeight;
 
   const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  console.log(x, y);
 }
 
 hero.addEventListener("mousemove", shadow);
